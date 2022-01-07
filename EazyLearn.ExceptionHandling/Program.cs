7bfc25a87c676754a35541499dea463135701510
SULFIKKAR E S <sulfikkeres369@hotmail.com>
2022-01-07 23:02:38 +0530

    https://github.com/SulfikkarES

--- EazyLearn.ExceptionHandling/Program.cs
+++ EazyLearn.ExceptionHandling/Program.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿#region Copyright EazyLearn
+//
+// All rights are reserved. Reproduction or transmission in whole or in part, 
+// in any form or by any means, electronic, mechanical or otherwise, is
+// prohibited without the prior written consent of the copyright owner.
+//
+// Filename        : C#ExceptionHandling.cs
+// Purpose         : To practice exception handling in c# 
+// Creation Date   : 07-10-2021
+// Author          : Sulfikkar E S
+//
+// Change History  :
+// Changed by      :              
+// Date            : 
+// Purpose         :
+//
+#endregion Copyright 
+
+#region Included namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq; 
 using System.Text;
 using System.Threading.Tasks;
+#endregion included namespaces
 
 namespace EazyLearn.ExceptionHandling
 {
